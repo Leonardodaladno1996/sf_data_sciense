7183768a5e3147f82136537dafa5074c70dadf04
Leonardo <Leonardodaladno@yandex.ru>
2024-09-13 13:52:33 +0300

    this is a commit, bitch !

---
+++ pointers_algo_2.py
@@ -0,0 +1,55 @@
+seq = input()
+
+dict_quotes = {')': '(',']': '[','}': '{'}
+
+
+stack = []
+
+answer = 'yes'
+
+for symbol in seq:
+
+    if symbol in dict_quotes.values():
+    
+        stack.append(symbol)
+
+    else:
+    
+        if not stack or stack[-1] != dict_quotes[symbol]:
+        
+            answer = 'no'
+            
+            break
+         
+        else:
+             
+            stack.pop()
+
+
+if stack:
+
+    answer = 'no'
+
+
+print(answer)
+
+        
+
+
+
+          
+
+
+                  
+    
+    
+    
+
+    
+    
+
+            
+
+
+    
+    
